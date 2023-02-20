6118ff6ffdc2f1d949c8513dfca7c982f1c57f11
Eduardo Vieira <eduardohvieira05@gmail.com>
2023-02-20 03:36:48 -0300

    api-commit

---
+++ src/pages/api/get-promo.js
@@ -0,0 +1,8 @@
+export default async (req, res) => {
+  res.end(
+    JSON.stringify({
+      showCoupon: false,
+      message: "Coupon message"
+    })
+  );
+};
--- src/pages/pesquisa.js
+++ src/pages/pesquisa.js
@@ -2,6 +2,22 @@ import React from "react";
 import Link from "next/link";
 
 const pesquisa = () => {
+  const save = async () => {
+    const form = {
+      Nome: "ccc",
+      Email: "bbb",
+      Whatsapp: "aaa",
+    };
+    try {
+      const response = await fetch("/api/save", {
+        method: "POST",
+        body: JSON.stringify(form),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {}
+  };
+
   return (
     <div className="pt-6">
       <h1 className="text-center font-bold my-4 text-2xl">Críticas ou sugestões</h1>
@@ -13,9 +29,11 @@ const pesquisa = () => {
       <div className="w-1/5 mx-auto ">
         <label className="font-bold">Seu nome:</label>
         <input type="text" className="p-4 block shadow bg-blue-100 my-2 rounded" />
+        <button className="bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow" onClick={save}>
+          Salvar
+        </button>
       </div>
     </div>
   );
 };
-
 export default pesquisa;
