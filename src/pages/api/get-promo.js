ff017ba419a7a40700c3e4404a87d19053ebcd82
Eduardo Vieira <eduardohvieira05@gmail.com>
2023-02-21 01:17:59 -0300

    cell-fix

--- src/pages/api/get-promo.js
+++ src/pages/api/get-promo.js
@@ -8,9 +8,9 @@ export default async (req, res) => {
     await doc.useServiceAccountAuth(credentials);
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[2];
-    await sheet.loadCells("A3:B3");
-    const mostrarPromocaoCell = sheet.getCell(2, 0);
-    const textoCell = sheet.getCell(2, 1);
+    await sheet.loadCells("A2:B2");
+    const mostrarPromocaoCell = sheet.getCell(1, 0);
+    const textoCell = sheet.getCell(1, 1);
 
     res.end(
       JSON.stringify({
